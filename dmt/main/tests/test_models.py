853ba7e034851e7188c9790aa1346f20c9bf0179
Anders Pearson <anders@columbia.edu>
2013-07-26 15:46:38 -0400

    user model tests

--- dmt/main/tests/test_models.py
+++ dmt/main/tests/test_models.py
@@ -1,6 +1,12 @@
 from django.test import TestCase
+from .factories import UserFactory
 
 
-class BasicTest(TestCase):
-    def test_dummy(self):
-        assert True
+class UserModelTest(TestCase):
+    def test_gau(self):
+        u = UserFactory()
+        self.assertEqual(u.get_absolute_url(), "/user/%s/" % u.username)
+
+    def test_unicode(self):
+        u = UserFactory()
+        self.assertEqual(str(u), u.fullname)
