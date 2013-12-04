2307e96d83a87ff65970289507b3ce04262765ee
Anders Pearson <anders@columbia.edu>
2013-12-04 15:44:40 -0500

    switch to default context object names

--- dmt/main/views.py
+++ dmt/main/views.py
@@ -5,19 +5,15 @@ from .models import Project, Milestone, Item
 
 class AllProjectsView(ListView):
     model = Project
-    context_object_name = 'projects'
 
 
 class ProjectView(DetailView):
     model = Project
-    context_object_name = 'project'
 
 
 class MilestoneView(DetailView):
     model = Milestone
-    context_object_name = 'milestone'
 
 
 class ItemView(DetailView):
     model = Item
-    context_object_name = 'item'
