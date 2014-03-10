59975a6c5ff6cbb46582756832d0a887d4ff86b1
Anders Pearson <anders@columbia.edu>
2014-03-10 14:43:34 -0400

    get more of the graphite stats reporting code under test coverage

--- dmt/main/tasks.py
+++ dmt/main/tasks.py
@@ -8,67 +8,86 @@ from .models import User
 from dmt.claim.models import Claim
 
 
+def get_item_counts_by_status():
+    data = dict()
+    data['total'] = Item.objects.count()
+    data['open'] = Item.objects.filter(status='OPEN').count()
+    data['inprogress'] = Item.objects.filter(status='INPROGRESS').count()
+    data['resolved'] = Item.objects.filter(status='RESOLVED').count()
+    data['closed'] = Item.objects.filter(status='CLOSED').count()
+    data['verified'] = Item.objects.filter(status='VERIFIED').count()
+    return data
+
+
 @periodic_task(run_every=crontab(hour='*', minute='*', day_of_week='*'))
 def item_stats_report():
     start = time.time()
-    statsd.gauge("items.total", Item.objects.count())
-    statsd.gauge("items.open", Item.objects.filter(status='OPEN').count())
-    statsd.gauge("items.inprogress",
-                 Item.objects.filter(status='INPROGRESS').count())
-    statsd.gauge("items.resolved",
-                 Item.objects.filter(status='RESOLVED').count())
-    statsd.gauge("items.closed",
-                 Item.objects.filter(status='CLOSED').count())
-    statsd.gauge("items.verified",
-                 Item.objects.filter(status='VERIFIED').count())
+    d = get_item_counts_by_status()
+    statsd.gauge("items.total", d['total'])
+    statsd.gauge("items.open", d['open'])
+    statsd.gauge("items.inprogress", d['inprogress'])
+    statsd.gauge("items.resolved", d['resolved'])
+    statsd.gauge("items.closed", d['closed'])
+    statsd.gauge("items.verified", d['verified'])
     end = time.time()
     statsd.timing('celery.item_stats_report', int((end - start) * 1000))
+
+
+def item_counts():
+    d = dict()
+    d['total_open_items'] = Item.objects.filter(
+        status__in=['OPEN', 'INPROGRESS'])
+    d['open_sm_items'] = Item.objects.filter(
+        status__in=['OPEN', 'INPROGRESS'],
+        milestone__name='Someday/Maybe')
+    d['open_sm_count'] = d['total_open_items'].count()
+    total_hours_estimated = interval_sum(
+        [i.estimated_time for i in d['total_open_items']]
+    ).total_seconds() / 3600.
+
+    sm_hours_estimated = interval_sum(
+        [i.estimated_time for i in d['open_sm_items']]
+    ).total_seconds() / 3600.
+
+    d['estimates_sm'] = int(sm_hours_estimated)
+    d['estimates_non_sm'] = int(
+        total_hours_estimated - sm_hours_estimated)
+    return d
 
 
 @periodic_task(run_every=crontab(hour='*', minute='*', day_of_week='*'))
 def estimates_report():
     start = time.time()
-    total_open_items = Item.objects.filter(
-        status__in=['OPEN', 'INPROGRESS'])
-    open_sm_items = Item.objects.filter(
-        status__in=['OPEN', 'INPROGRESS'],
-        milestone__name='Someday/Maybe')
+    d = item_counts()
     # item counts
-    statsd.gauge('items.open.sm', open_sm_items.count())
+    statsd.gauge('items.open.sm', d['open_sm_count'])
 
     # hour estimates
-    total_hours_estimated = interval_sum(
-        [i.estimated_time for i in total_open_items]
-    ).total_seconds() / 3600.
-
-    sm_hours_estimated = interval_sum(
-        [i.estimated_time for i in open_sm_items]
-    ).total_seconds() / 3600.
-
-    statsd.gauge('estimates.sm', int(sm_hours_estimated))
-    statsd.gauge('estimates.non_sm',
-                 int(total_hours_estimated - sm_hours_estimated))
+    statsd.gauge('estimates.sm', d['estimates_sm'])
+    statsd.gauge('estimates.non_sm', d['estimates_non_sm'])
 
     end = time.time()
     statsd.timing('celery.estimates_report', int((end - start) * 1000))
 
 
-@periodic_task(run_every=crontab(hour='*', minute='*', day_of_week='*'))
-def hours_logged_report():
-    start = time.time()
+def hours_logged(weeks=1):
     now = datetime.now()
-    one_week_ago = now - timedelta(weeks=1)
+    one_week_ago = now - timedelta(weeks=weeks)
     # active projects
     times_logged = ActualTime.objects.filter(
         completed__gt=one_week_ago).select_related(
         'item', 'resolver', 'item__milestone',
         'item__milestone__project')
-    statsd.gauge(
-        "hours.one_week",
-        int(
-            interval_sum(
-                [a.actual_time for a in times_logged]
-            ).total_seconds() / 3600.))
+    return int(
+        interval_sum(
+            [a.actual_time for a in times_logged]
+        ).total_seconds() / 3600.)
+
+
+@periodic_task(run_every=crontab(hour='*', minute='*', day_of_week='*'))
+def hours_logged_report():
+    start = time.time()
+    statsd.gauge("hours.one_week", hours_logged())
     end = time.time()
     statsd.timing('celery.hours_logged_report', int((end - start) * 1000))
 
---
+++ dmt/main/tests/test_tasks.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from dmt.main.tasks import (
+    get_item_counts_by_status, item_counts, hours_logged)
+
+
+class TestHelpers(TestCase):
+    def test_get_item_counts_by_status(self):
+        d = get_item_counts_by_status()
+        self.assertEqual(d['total'], 0)
+
+    def test_item_counts(self):
+        d = item_counts()
+        self.assertTrue('total_open_items' in d)
+        self.assertTrue('estimates_sm' in d)
+        self.assertTrue('estimates_non_sm' in d)
+
+    def test_hours_logged(self):
+        self.assertEqual(hours_logged(), 0)
