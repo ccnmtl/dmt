3f2cdbde472676f9c734ae446d08c98160c9ed42
Anders Pearson <anders@columbia.edu>
2014-04-23 16:24:40 -0400

    hallelujah, django 1.6 is better at finding tests, so we don't need to import stuff in __init__.py anymore

--- dmt/claim/tests/__init__.py
+++ dmt/claim/tests/__init__.py
@@ -1,4 +1,1 @@
-# flake8: noqa
 
-from test_models import *
-from test_views import *
--- dmt/main/tests/__init__.py
+++ dmt/main/tests/__init__.py
@@ -1,4 +1,1 @@
-# flake8: noqa
-from test_models import *
-from test_views import *
-from test_tasks import *
+
