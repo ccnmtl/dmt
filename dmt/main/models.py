29125a5a67b58cff044e3dc8484d35eb9685940c
Nik Nyby <nnyby@columbia.edu>
2014-07-18 13:35:04 -0400

    Add back Project.all_projects_by_last_mod()
    disappeared in a merge conflict

--- dmt/main/models.py
+++ dmt/main/models.py
@@ -531,6 +531,19 @@ ORDER BY hours_logged DESC;
         """, [start, end])
         return projects
 
+    @staticmethod
+    def all_projects_by_last_mod():
+        projects = Project.objects.raw("""
+SELECT
+    m.pid,
+    date_trunc('minute',max(i.last_mod)) AS last_mod
+FROM milestones m
+LEFT OUTER JOIN items i
+    ON m.mid = i.mid
+GROUP BY m.pid
+        """)
+        return projects
+
 
 class Document(models.Model):
     did = models.AutoField(primary_key=True)
