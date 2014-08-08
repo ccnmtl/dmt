5bf949e55a11858aa132ef6cbbad5355a8be4686
Nik Nyby <nnyby@columbia.edu>
2014-08-08 09:57:42 -0400

    Add ReportFileGenerator
    Generalized report generation to work with different reports.

--- dmt/report/views.py
+++ dmt/report/views.py
@@ -1,8 +1,7 @@
 from datetime import datetime, timedelta
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView, View
-import StringIO
 from dmt.claim.models import Claim
 from dmt.main.models import User, Item, Milestone
 from dmt.main.views import LoggedInMixin
@@ -11,6 +10,7 @@ from .models import (
     ActiveProjectsCalculator, StaffReportCalculator,
     WeeklySummaryReportCalculator)
 from .mixins import PrevNextWeekMixin
+from .utils import ReportFileGenerator
 
 
 class ActiveProjectsView(LoggedInMixin, TemplateView):
@@ -36,70 +36,11 @@ class ActiveProjectsView(LoggedInMixin, TemplateView):
 
 
 class ActiveProjectsExportView(LoggedInMixin, View):
-    def export_csv(self, data, columns, filename):
-        """
-        Generates the report as a CSV. Returns an HttpResponse.
-        """
-        import unicodecsv
-
-        filename += '.csv'
-        response = HttpResponse(content_type='text/csv')
-        response['Content-Disposition'] = \
-            "attachment; filename=\"%s\"" % (filename)
-
-        writer = unicodecsv.writer(response)
-        writer.writerow(columns)
-        for p in data['projects']:
-            writer.writerow([p.pid, p.name, p.projnum, p.last_worked_on,
-                             p.status, p.caretaker,
-                             interval_to_hours(p.hours_logged)])
-        return response
-
-    def export_excel(self, data, columns, filename):
-        """
-        Generates the report as an MS Excel file. Returns an HttpResponse.
-        """
-        import xlsxwriter
-
-        # Excel
-        filename += '.xlsx'
-        output = StringIO.StringIO()
-        response = HttpResponse(
-            content_type='application/vnd.openxmlformats-' +
-            'officedocument.spreadsheetml.sheet')
-        response['Content-Disposition'] = \
-            "attachment; filename=\"%s\"" % (filename)
-
-        workbook = xlsxwriter.Workbook(output, {'in_memory': True})
-        worksheet = workbook.add_worksheet()
-        bold = workbook.add_format({'bold': True})
-        for i in range(len(columns)):
-            worksheet.write(0, i, columns[i], bold)
-
-        row = 1
-        for p in data['projects']:
-            # Some of these table cells have problems when being written
-            # with write_row(), so we stub them out and write to those
-            # cells with an explicit type.
-            worksheet.write_row(row, 0,
-                                [p.pid, p.name, p.projnum, None, p.status,
-                                 None, interval_to_hours(p.hours_logged)])
-            worksheet.write_string(row, 3, str(p.last_worked_on))
-            worksheet.write_string(row, 5, str(p.caretaker))
-            row += 1
-
-        workbook.close()
-
-        output.seek(0)
-        response.write(output.read())
-
-        return response
-
     def get(self, request, **kwargs):
         now = datetime.now()
         filename = "active-projects-%s-%s-%s" % (now.year, now.month, now.day)
-        columns = ['ID', 'Name', 'Project Number', 'Last worked on',
-                   'Project Status', 'Caretaker', 'Hours logged']
+        column_names = ['ID', 'Name', 'Project Number', 'Last worked on',
+                        'Project Status', 'Caretaker', 'Hours logged']
 
         days = 31
         if kwargs['days']:
@@ -107,10 +48,13 @@ class ActiveProjectsExportView(LoggedInMixin, View):
         calc = ActiveProjectsCalculator()
         data = calc.calc(days)
 
-        if kwargs['format'] == 'csv':
-            return self.export_csv(data, columns, filename)
-        else:
-            return self.export_excel(data, columns, filename)
+        rows = [[x.pid, x.name, x.projnum, x.last_worked_on, x.status,
+                 x.caretaker, interval_to_hours(x.hours_logged)]
+                for x in data['projects']]
+
+        generator = ReportFileGenerator()
+        return generator.generate(
+            column_names, rows, filename, kwargs['format'])
 
 
 class YearlyReviewView(LoggedInMixin, View):
