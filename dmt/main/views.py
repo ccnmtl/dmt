d17c6dd8d1f18b7c190f35ad837c1a2b072218e4
Nik Nyby <nnyby@columbia.edu>
2014-11-07 14:29:20 -0500

    Remove mdx_linkify. It was linkifying strings too liberally.
    PMT #96878

--- dmt/main/views.py
+++ dmt/main/views.py
@@ -127,7 +127,7 @@ class AddCommentView(LoggedInMixin, View):
         body = request.POST.get('comment', u'')
         if body == '':
             return HttpResponseRedirect(item.get_absolute_url())
-        item.add_comment(user, markdown.markdown(body, extensions=['linkify']))
+        item.add_comment(user, markdown.markdown(body))
         item.touch()
         item.update_email(body, user)
         log_time(item, user, request)
@@ -140,8 +140,7 @@ class ResolveItemView(LoggedInMixin, View):
         item = get_object_or_404(Item, pk=pk)
         user = get_object_or_404(Claim, django_user=request.user).pmt_user
         r_status = request.POST.get('r_status', u'FIXED')
-        comment = markdown.markdown(request.POST.get('comment', u''),
-                                    extensions=['linkify'])
+        comment = markdown.markdown(request.POST.get('comment', u''))
         if (item.assigned_to.username == item.owner.username and
                 item.owner.username == user.username):
             # streamline self-assigned item verification
@@ -161,8 +160,7 @@ class InProgressItemView(LoggedInMixin, View):
     def post(self, request, pk):
         item = get_object_or_404(Item, pk=pk)
         user = get_object_or_404(Claim, django_user=request.user).pmt_user
-        comment = markdown.markdown(request.POST.get('comment', u''),
-                                    extensions=['linkify'])
+        comment = markdown.markdown(request.POST.get('comment', u''))
         item.mark_in_progress(user, comment)
         item.touch()
         item.update_email("marked as in progress\n----\n"
@@ -177,8 +175,7 @@ class VerifyItemView(LoggedInMixin, View):
     def post(self, request, pk):
         item = get_object_or_404(Item, pk=pk)
         user = get_object_or_404(Claim, django_user=request.user).pmt_user
-        comment = markdown.markdown(request.POST.get('comment', u''),
-                                    extensions=['linkify'])
+        comment = markdown.markdown(request.POST.get('comment', u''))
         item.verify(user, comment)
         item.touch()
         item.update_email("verified\n-----\n"
@@ -193,8 +190,7 @@ class ReopenItemView(LoggedInMixin, View):
     def post(self, request, pk):
         item = get_object_or_404(Item, pk=pk)
         user = get_object_or_404(Claim, django_user=request.user).pmt_user
-        comment = markdown.markdown(request.POST.get('comment', u''),
-                                    extensions=['linkify'])
+        comment = markdown.markdown(request.POST.get('comment', u''))
         item.reopen(user, comment)
         item.touch()
         item.update_email("reopened\n-----\n"
@@ -212,8 +208,7 @@ class ReassignItemView(LoggedInMixin, View):
         assigned_to = get_object_or_404(
             User,
             username=request.POST.get('assigned_to', ''))
-        comment = markdown.markdown(request.POST.get('comment', u''),
-                                    extensions=['linkify'])
+        comment = markdown.markdown(request.POST.get('comment', u''))
         item.reassign(user, assigned_to, comment)
         item.touch()
         item.update_email("reassigned\n----\n"
@@ -230,8 +225,7 @@ class ChangeOwnerItemView(LoggedInMixin, View):
         owner = get_object_or_404(
             User,
             username=request.POST.get('owner', ''))
-        comment = markdown.markdown(request.POST.get('comment', u''),
-                                    extensions=['linkify'])
+        comment = markdown.markdown(request.POST.get('comment', u''))
         item.change_owner(user, owner, comment)
         item.touch()
         item.update_email("owner changed\n-----\n"
