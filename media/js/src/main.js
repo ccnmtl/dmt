35ab822d7617f4209d71ebf0e3af4acff25a5d4b
Nik Nyby <nnyby@columbia.edu>
2014-12-05 11:58:05 -0500

    Increase bloodhound fetch limit to 10  (fix PMT #98169)
    Also, moves the typeahead code out of the templates.

--- media/js/src/main.js
+++ media/js/src/main.js
@@ -5,6 +5,8 @@ require([
     '../libs/backbone/backbone-min',
     '../libs/bootstrap-datepicker/bootstrap-datepicker.min',
     '../libs/remarkable/remarkable',
+    '../libs/typeahead/bloodhound.min',
+    '../libs/typeahead/typeahead.jquery.min',
 
     // src
     'utils/markdown_preview',
@@ -15,12 +17,53 @@ require([
     'forms/project_add_bug_form',
     'item'
 ], function() {
+
+    var projects = new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        limit: 10,
+        prefetch: {
+            url: '/drf/projects/',
+            filter: function (projects) {
+                return $.map(projects.results, function (data) {
+                    return {
+                        name: data.name,
+                        pid: data.pid
+                    };
+                });
+            }
+        },
+        remote: {
+            url: '/drf/projects/?search=%QUERY',
+            filter: function (projects) {
+                return $.map(projects.results, function (data) {
+                    return {
+                        name: data.name,
+                        pid: data.pid
+                    };
+                });
+            }
+        }
+    });
+    projects.initialize();
+
     $(document).ready(function() {
         if (typeof $().datepicker === 'function') {
             $('input[name=target_date]').datepicker({
                 autoclose: true,
                 format: 'yyyy-mm-dd',
                 todayHighlight: true
+            });
+        }
+
+        if (typeof $().typeahead === 'function') {
+            $("#project-input").typeahead(null, {
+                name: 'results',
+                displayKey: 'name',
+                source: projects.ttAdapter()
+            });
+            $("#project-input").on("typeahead:selected", function (object, datum) {
+                $("#tracker-pid-input").val(datum.pid);
             });
         }
     });
