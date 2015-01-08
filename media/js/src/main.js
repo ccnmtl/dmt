cf5202375278595ccb587e4e86e34d4eedfe3e1e
Nik Nyby <nnyby@columbia.edu>
2015-01-08 16:32:48 -0500

    Add CSRF token to AJAX requests
    Also updates jquery version to 1.11.2

--- media/js/src/main.js
+++ media/js/src/main.js
@@ -1,6 +1,7 @@
 require([
     // libs
     '../libs/jquery/jquery-min',
+    '../libs/jquery.cookie.min',
     '../libs/underscore/underscore-min',
     '../libs/backbone/backbone-min',
     '../libs/bootstrap-datepicker/bootstrap-datepicker.min',
@@ -17,6 +18,40 @@ require([
     'forms/project_add_bug_form',
     'item'
 ], function() {
+    var csrftoken = $.cookie('csrftoken');
+
+    // The following is from
+    // https://docs.djangoproject.com/en/1.7/ref/contrib/csrf/#ajax
+    function csrfSafeMethod(method) {
+        // these HTTP methods do not require CSRF protection
+        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    }
+    function sameOrigin(url) {
+        // test that a given url is a same-origin URL
+        // url could be relative or scheme relative or absolute
+        var host = document.location.host; // host + port
+        var protocol = document.location.protocol;
+        var srOrigin = '//' + host;
+        var origin = protocol + srOrigin;
+        // Allow absolute or scheme relative URLs to same origin
+        return (url == origin ||
+                url.slice(0, origin.length + 1) == origin + '/') ||
+            (url == srOrigin ||
+             url.slice(0, srOrigin.length + 1) == srOrigin + '/') ||
+            // or any other URL that isn't scheme relative or absolute
+            // i.e relative.
+            !(/^(\/\/|http:|https:).*/.test(url));
+    }
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            if (!csrfSafeMethod(settings.type) && sameOrigin(settings.url)) {
+                // Send the token to same-origin, relative URLs only.
+                // Send the token only if the method warrants CSRF protection
+                // Using the CSRFToken value acquired earlier
+                xhr.setRequestHeader('X-CSRFToken', csrftoken);
+            }
+        }
+    });
 
     var projects = new Bloodhound({
         datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
