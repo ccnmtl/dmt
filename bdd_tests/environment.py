be7118f9f619e4848ae7c8f119179c3b0b6c816c
Anders Pearson <anders@columbia.edu>
2015-03-02 14:32:42 +0100

    switch to jump straight into a debugger on step failure

--- bdd_tests/environment.py
+++ bdd_tests/environment.py
@@ -1,6 +1,12 @@
 import urlparse
 from splinter import Browser
 
+# change this to True and you will get dumped into
+# an ipdb shell when a step fails. Good for debugging.
+# remember to set it back to False before this goes to travis
+# though...
+BEHAVE_DEBUG_ON_ERROR = False
+
 
 def before_all(context):
     host = context.host = 'localhost'
@@ -23,3 +29,9 @@ def before_scenario(context, scenario):
 
 def after_scenario(context, scenario):
     pass
+
+
+def after_step(context, step):
+    if BEHAVE_DEBUG_ON_ERROR and step.status == "failed":
+        import ipdb
+        ipdb.post_mortem(step.exc_traceback)
