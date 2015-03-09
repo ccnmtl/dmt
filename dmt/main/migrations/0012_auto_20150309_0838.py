33c0765fa6b28330275115563175cb26ddc4fff7
Anders Pearson <anders@columbia.edu>
2015-03-09 14:16:38 +0100

    data migration to backfill node author -> user

---
+++ dmt/main/migrations/0012_auto_20150309_0838.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+def backfill_users(apps, schema_editor):
+    Node = apps.get_model('main', 'Node')
+
+    for node in Node.objects.all():
+        if node.user is not None:
+            # don't need to backfill it
+            continue
+        node.user = node.author.userprofile
+        node.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('main', '0011_auto_20150206_1048'),
+    ]
+
+    operations = [
+    ]
