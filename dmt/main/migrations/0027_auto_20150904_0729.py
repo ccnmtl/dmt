af9f83ed2207f3c1a48c0e4bf25f93fde8062570
Anders Pearson <anders@columbia.edu>
2015-09-04 13:34:06 +0200

    migration to backfill NotifyProject.user

---
+++ dmt/main/migrations/0027_auto_20150904_0729.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+def notify_project_username_to_user(apps, schema_editor):
+    NotifyProject = apps.get_model("main", "NotifyProject")
+    for np in NotifyProject.objects.all():
+        np.user = np.username.user
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('main', '0026_auto_20150827_0617'),
+    ]
+
+    operations = [
+        migrations.RunPython(notify_project_username_to_user)
+    ]
