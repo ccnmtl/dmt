33292be86277cb83bd90d10cd3374c6b30d3cc8d
Nik Nyby <nnyby@columbia.edu>
2016-02-01 14:27:34 -0500

    Autofocus "Add time" input when modal opens
    Also cleaned up some formatting in views/item.js

--- media/js/src/views/item.js
+++ media/js/src/views/item.js
@@ -11,37 +11,36 @@ define([
         return;
     }
 
-    var ItemView = Backbone.View.extend(
-        {
-            tagName: 'div',
-            template: _.template($('#item-template').html()),
-            events: {
-                'change #input_notification': 'updateNotificationStatus',
-            },
-            initialize: function() {
-                this.notify = new Notify(
-                    {iid: this.model.get('iid')});
+    var ItemView = Backbone.View.extend({
+        tagName: 'div',
+        template: _.template($('#item-template').html()),
+        events: {
+            'change #input_notification': 'updateNotificationStatus',
+        },
+        initialize: function() {
+            this.notify = new Notify(
+                {iid: this.model.get('iid')});
 
-                $('#input_notification')
-                    .on('change',
-                        {notify: this.notify},
-                        this.updateNotificationStatus);
-            },
-            render: function() {
-                this.$el.html(this.template(this.model.attributes));
-                return this;
-            },
-            updateNotificationStatus: function(e) {
-                var isEnabled = e.target.checked;
-                var notify = e.data.notify;
-                if (isEnabled) {
-                    // create a notify object
-                    notify.save();
-                } else {
-                    // delete the notify
-                    notify.destroy();
-                }
+            $('#input_notification')
+                .on('change',
+                    {notify: this.notify},
+                    this.updateNotificationStatus);
+        },
+        render: function() {
+            this.$el.html(this.template(this.model.attributes));
+            return this;
+        },
+        updateNotificationStatus: function(e) {
+            var isEnabled = e.target.checked;
+            var notify = e.data.notify;
+            if (isEnabled) {
+                // create a notify object
+                notify.save();
+            } else {
+                // delete the notify
+                notify.destroy();
             }
-        });
+        }
+    });
     return ItemView;
 });
