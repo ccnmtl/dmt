3bd00329dafce0a78e0ec1318f3a8f32e138b122
Nik Nyby <nnyby@columbia.edu>
2016-02-10 16:49:57 -0500

    Fix filter tabs bug - PMT #105010

--- media/js/src/utils/bootstrap_tabs.js
+++ media/js/src/utils/bootstrap_tabs.js
@@ -2,19 +2,21 @@
 // because it needs the Bootstrap-aware jQuery object.
 
 $(document).ready(function() {
+    var $parent = $('ul.nav.homepage-tabs,ul.nav.project-detail-tabs');
+
     // Fix the problem where the window's scroll is below the top
     // of the page when navigating to a tab via anchor tag and click.
-    $('ul.nav a').on('shown.bs.tab', function() {
+    $parent.find('a').on('shown.bs.tab', function() {
         $(window).scrollTop(0);
     });
 
     var hash = window.location.hash;
     if (hash) {
-        var $el = $('ul.nav a[href="' + hash + '"]');
+        var $el = $parent.find('a[href="' + hash + '"]');
         $el.tab('show');
     }
 
-    $('.nav-tabs a').click(function(e) {
+    $parent.find('a').click(function(e) {
         $(this).tab('show');
         window.location.hash = this.hash;
     });
