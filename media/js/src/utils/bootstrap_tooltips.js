ce087b67d702a64680bbf33a3f4436eefec70a78
Nik Nyby <nnyby@columbia.edu>
2016-02-16 10:35:39 -0500

    Add note explaining what recent projects are - PMT #105260

---
+++ media/js/src/utils/bootstrap_tooltips.js
@@ -0,0 +1,8 @@
+// This file is loaded with a <script> tag, not with requirejs
+// because it needs the Bootstrap-aware jQuery object.
+
+$(document).ready(function() {
+    $('[data-toggle="tooltip"]').tooltip({
+        animation: false
+    });
+});
