5b9c472ad2e1dff38e3de13ec51beacc02b7b90b
Nik Nyby <nnyby@columbia.edu>
2016-03-08 13:31:01 -0500

    Add management command to populate ActualTime uuids

---
+++ dmt/main/management/commands/populate_actualtime_uuids.py
@@ -0,0 +1,11 @@
+import uuid
+from django.core.management.base import BaseCommand
+from dmt.main.models import ActualTime
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        """Assign uuids to ActualTimes that don't have one."""
+        for time in ActualTime.objects.filter(uuid=None):
+            time.uuid == uuid.uuid4()
+            time.save()
