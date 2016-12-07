9004b914064419091ff36dc4fb7614ec9b90f1f3
Anders Pearson <anders@columbia.edu>
2016-12-07 11:57:50 +0000

    add a <content:encoded> for status update feed items
    this is the "correct" way to include formatted content in an RSS feed.

--- dmt/main/feeds.py
+++ dmt/main/feeds.py
@@ -1,7 +1,23 @@
 from django.conf import settings
 from django.contrib.syndication.views import Feed
 from django.shortcuts import get_object_or_404
+from django.template import loader
+from django.utils.feedgenerator import Rss201rev2Feed
 from .models import Node, StatusUpdate, Project, Item
+
+
+class ExtendedRSSFeed(Rss201rev2Feed):
+    """
+    Create a type of RSS feed that has content:encoded elements.
+    """
+    def root_attributes(self):
+        attrs = super(ExtendedRSSFeed, self).root_attributes()
+        attrs['xmlns:content'] = 'http://purl.org/rss/1.0/modules/content/'
+        return attrs
+
+    def add_item_elements(self, handler, item):
+        super(ExtendedRSSFeed, self).add_item_elements(handler, item)
+        handler.addQuickElement(u'content:encoded', item['content_encoded'])
 
 
 class ForumFeed(Feed):
@@ -32,6 +48,7 @@ class ForumFeed(Feed):
 
 
 class StatusUpdateFeed(Feed):
+    feed_type = ExtendedRSSFeed
     title = "PMT Status Updates"
     description = "recent status updates"
     description_template = "feeds/status_item_description.html"
@@ -54,6 +71,11 @@ class StatusUpdateFeed(Feed):
 
     def item_pubdate(self, item):
         return item.added
+
+    def item_extra_kwargs(self, item):
+        description_tmp = loader.get_template(self.description_template)
+        description = description_tmp.render(dict(obj=item), None)
+        return {'content_encoded': description}
 
 
 class ProjectFeed(Feed):
