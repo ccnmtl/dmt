daf8561184f5545e6ee1c4673d28dba4c8f99afc
Anders Pearson <anders@columbia.edu>
2017-01-23 12:00:45 +0000

    make dashboard graph timespan configurable in settings
    since moved our graphite server, we don't have the old metrics anymore,
    so the graphs should be adjusted now and then until we get a year or so
    of data up there. This should make it a little simpler.

--- dmt/main/contextprocessors.py
+++ dmt/main/contextprocessors.py
@@ -5,3 +5,9 @@ def graphite_base_processor(request):
     return {
         'GRAPHITE_BASE': settings.GRAPHITE_BASE,
     }
+
+
+def dashboard_graph_timespan(request):
+    return {
+        'DASHBOARD_GRAPH_TIMESPAN': settings.DASHBOARD_GRAPH_TIMESPAN,
+    }
